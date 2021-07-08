fbbaad009b8f11548bc0f93a84f0217909445ae4
Murtazin <renatmurtazin@yahoo.com>
2021-07-08 21:23:32 +0300

    commit

--- Tabu/MainScreen/InfoViewController.swift
+++ Tabu/MainScreen/InfoViewController.swift
@@ -43,28 +43,30 @@ class InfoViewController: UIViewController {
         pageLabel.text = "1/5"
     }
     @IBAction func previousAction(_ sender: Any) {
+        nextButton.isHidden = false
         if step > 1 {
             step -= 1
             switch step {
             case 1:
                 imageView.image = UIImage(named: "tabooLogo")
-                textField.text = "Overview: \nTaboo is a verbal game played with two teams of three players or more, a deck of 502 guess cards with a set of taboo words, and a battery operated buzzer. The buzzer requires two AA batteries."
+                textField.text = "Overview: \nTaboo is a verbal game played with two teams a deck of 502 guess cards with a set of taboo words"
                 pageLabel.text = "\(step) / \(countOfPages)"
+                previousButton.isHidden = true
             case 2:
                 imageView.image = UIImage(named: "card")
-                textField.text = "Game setup: \nSix or more players are divided up into two teams, with one team referred to as Team A and the other team referred to as Team B.504 cards are placed in the card holder and Team A selects a person in their group to be the Clue-giver. This person takes the card holder and places the first card away from his team so that they cannot see it. Clue cards have the clue word on the top of the card and the taboo words listed below the clue word. Clue-givers then start and keep the timer and must get their team to say the guess-word on the card without using one of the taboo words in one of their clues. If a taboo word is used then Team B must sound the buzzer which penalizes Team A by causing the current guess card to go into the discard pile.Play is continued until time runs out. Each time a teammate successfully guesses a clue-word, a new card is placed upon the old and each card in this pile represents one point for Team A."
+                textField.text = "Preparation for the game: \nThe players are divided into two teams: one team is called team A and the other is called team B. 502 cards are placed in the card holder. It is the turn of the player from team A. The hint player takes a card containing 5 taboo words, that is, words that cannot be named when explaining just one word written on the card. The game continues until the time runs out. Each time a teammate successfully guesses a keyword, a new card is placed on top of the old one, and each card in that pile represents one point for Team A. In the event that a player from the first team says the word “taboo,” a point is awarded to the second team. There comes a change of teams."
                 pageLabel.text = "\(step) / \(countOfPages)"
             case 3:
                 imageView.image = UIImage(named: "watches")
-                textField.text = "Gameplay and Scoring: \nThe team not giving clues must watch for Taboos or other violations of the rules. If they successfully spot a rule being broken, they may use their buzzer to interrupt play and very briefly explain the infraction. Clue-givers must place the current card into the discard pile and draw a new one. Each card in the discard pile represents one point for the other team. Once time runs out, the teams are swapped and roles are reversed. Once each player has taken a turn as clue-give, r the game ends. If teams are uneven, a player on the smaller team will be clue-giver twice."
+                textField.text = "Gameplay and scoring: \nin the event that a player answers correctly, the point goes to his team. In the case when a player, prompting his team-mate, names the word taboo, the point goes to the other team. Also, if the player does not cope with this card, he can change it. The previously listed rules are for all teams."
                 pageLabel.text = "\(step) / \(countOfPages)"
             case 4:
                 imageView.image = UIImage(named: "rules")
-                textField.text = "Rules: \n1. Clue-givers may not use any taboo words, including abbreviations and any part of the taboo word. 2. Clue-givers may not use sound effects or use gestures to indicate the clue word. 3. Clue-givers may pass on any card at any time, but the card is then placed in the discard pile. 4. When time is called, the final card does not go into the discard pile, but is instead removed from play."
+                textField.text = "Rules: \n1. Clue-givers may not use any taboo words, including abbreviations and any part of the taboo word. 2. Clue-givers may not use sound effects or use gestures to indicate the clue word. 3. Clue-givers may pass on any card at any time, but the card is then placed in the discard pile."
                 pageLabel.text = "\(step) / \(countOfPages)"
             case 5:
                 imageView.image = UIImage(named: "cards")
-                textField.text = "Scoring: \nEvery card stacked upon another in the card holder represents one point for Team A. Every card stacked in the discard pile represents one point for Team B. The final card in play when time runs out is worth no points for either team. At the end of each round points are tallied for each team, the winner is declared by the team with the most points."
+                textField.text = "Scoring: \nEvery card stacked upon another in the card holder represents one point for Team A. Every card stacked in the discard pile represents one point for Team B.  At the end of each round points are tallied for each team, the winner is declared by the team with the most points."
                 pageLabel.text = "\(step) / \(countOfPages)"
             default:
                 break
@@ -72,6 +74,7 @@ class InfoViewController: UIViewController {
         }
     }
     @IBAction func nextAction(_ sender: Any) {
+        previousButton.isHidden = false
         if step != countOfPages {
             step += 1
             switch step {
@@ -81,19 +84,20 @@ class InfoViewController: UIViewController {
 //                pageLabel.text = "\(step) / \(countOfPages)"
             case 2:
                 imageView.image = UIImage(named: "card")
-                textField.text = "Game setup: \nSix or more players are divided up into two teams, with one team referred to as Team A and the other team referred to as Team B.504 cards are placed in the card holder and Team A selects a person in their group to be the Clue-giver. This person takes the card holder and places the first card away from his team so that they cannot see it. Clue cards have the clue word on the top of the card and the taboo words listed below the clue word. Clue-givers then start and keep the timer and must get their team to say the guess-word on the card without using one of the taboo words in one of their clues. If a taboo word is used then Team B must sound the buzzer which penalizes Team A by causing the current guess card to go into the discard pile.Play is continued until time runs out. Each time a teammate successfully guesses a clue-word, a new card is placed upon the old and each card in this pile represents one point for Team A."
+                textField.text = "Preparation for the game: \nThe players are divided into two teams: one team is called team A and the other is called team B. 502 cards are placed in the card holder. It is the turn of the player from team A. The hint player takes a card containing 5 taboo words, that is, words that cannot be named when explaining just one word written on the card. The game continues until the time runs out. Each time a teammate successfully guesses a keyword, a new card is placed on top of the old one, and each card in that pile represents one point for Team A. In the event that a player from the first team says the word “taboo,” a point is awarded to the second team. There comes a change of teams."
                 pageLabel.text = "\(step) / \(countOfPages)"
             case 3:
                 imageView.image = UIImage(named: "watches")
-                textField.text = "Gameplay and Scoring: \nThe team not giving clues must watch for Taboos or other violations of the rules. If they successfully spot a rule being broken, they may use their buzzer to interrupt play and very briefly explain the infraction. Clue-givers must place the current card into the discard pile and draw a new one. Each card in the discard pile represents one point for the other team. Once time runs out, the teams are swapped and roles are reversed. Once each player has taken a turn as clue-give, r the game ends. If teams are uneven, a player on the smaller team will be clue-giver twice."
+                textField.text = "Gameplay and scoring: \nin the event that a player answers correctly, the point goes to his team. In the case when a player, prompting his team-mate, names the word taboo, the point goes to the other team. Also, if the player does not cope with this card, he can change it. The previously listed rules are for all teams."
                 pageLabel.text = "\(step) / \(countOfPages)"
             case 4:
                 imageView.image = UIImage(named: "rules")
-                textField.text = "Rules: \n1. Clue-givers may not use any taboo words, including abbreviations and any part of the taboo word. 2. Clue-givers may not use sound effects or use gestures to indicate the clue word. 3. Clue-givers may pass on any card at any time, but the card is then placed in the discard pile. 4. When time is called, the final card does not go into the discard pile, but is instead removed from play."
+                textField.text = "Rules: \n1. Clue-givers may not use any taboo words, including abbreviations and any part of the taboo word. 2. Clue-givers may not use sound effects or use gestures to indicate the clue word. 3. Clue-givers may pass on any card at any time, but the card is then placed in the discard pile."
                 pageLabel.text = "\(step) / \(countOfPages)"
             case 5:
                 imageView.image = UIImage(named: "cards")
-                textField.text = "Scoring: \nEvery card stacked upon another in the card holder represents one point for Team A. Every card stacked in the discard pile represents one point for Team B. The final card in play when time runs out is worth no points for either team. At the end of each round points are tallied for each team, the winner is declared by the team with the most points."
+                nextButton.isHidden = true
+                textField.text = "Scoring: \nEvery card stacked upon another in the card holder represents one point for Team A. Every card stacked in the discard pile represents one point for Team B.  At the end of each round points are tallied for each team, the winner is declared by the team with the most points."
                 pageLabel.text = "\(step) / \(countOfPages)"
             default:
                 break
